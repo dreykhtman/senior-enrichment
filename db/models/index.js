997a8e5f626002e6518000f2a840a2e03dffe076
Dima Reykhtman <neoxdth@gmail.com>
2017-10-14 17:03:21 -0400

    definded models

--- db/models/index.js
+++ db/models/index.js
@@ -1,8 +1,43 @@
 'use strict';
 
 // Require all the models
-	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db so any other part of the application could call db.model('user') OR db.models.user to get access to the `user` model.
-	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
-	// This is an acceptable pattern but it does have limitations in that if you change the name of the model you will have to change every time it is required everywhere
+  // Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db so any other part of the application could call db.model('user') OR db.models.user to get access to the `user` model.
+  // This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
+  // This is an acceptable pattern but it does have limitations in that if you change the name of the model you will have to change every time it is required everywhere
 
 // This is also probably a good place for you to set up your associations
+const Sequelize = require('sequelize');
+const db = require('../../db/index');
+
+const Student = db.define('student', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+const Campus = db.define('campus', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  image: {
+    type: Sequelize.STRING,
+    allowNull: false
+  }
+});
+
+Student.belongsTo(Campus, {as: 'residence'});
+
+module.exports = {
+  Student,
+  Campus
+};
